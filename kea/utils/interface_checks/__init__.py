116af3786b759ae0d1b077e96fe82eb8e8340f61
Toby <toby.gomersall@smartacoustics.co.uk>
2026-02-27 11:48:09 +0000

    update: Added a signal_out_valid signal to the synchronous_saturating_rounding_slicer. Added interface checks so blocks can check ports are correct.

---
+++ kea/utils/interface_checks/__init__.py
@@ -0,0 +1,6 @@
+from .interface_checks import check_bool_signal, check_intbv_signal
+from .verify_interface_checks_called import (
+    get_dut_check_bool_signal_call_arguments,
+    get_dut_check_intbv_signal_call_arguments,
+    verify_dut_called_check_bool_signal,
+    verify_dut_called_check_intbv_signal)
